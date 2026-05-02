e363c4f6b3addd89428bcdb1499ec143ef165337
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-05-01 19:05:55 -0600

    Mission assignment part 2
    I made the JavaScript change from light mode to dark mode

---
+++ mission/darkmode.js
@@ -0,0 +1,34 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // changes background black
+        document.querySelector("body").style.backgroundColor = "#242424";
+
+        // changes text to white
+        document.querySelector("h1").style.color = "white";
+        document.querySelector("p").style.color = "white";
+        document.querySelector(".italicized").style.color = "white";
+        document.querySelector("ol").style.color = "white";
+
+        // changes image
+        document.querySelector("img").setAttribute("src", "byui-logo-dark.png");
+    } else {
+        // changes background black
+        document.querySelector("body").style.backgroundColor = "white";
+
+        // changes text to white
+        document.querySelector("h1").style.color = "black";
+        document.querySelector("p").style.color = "black";
+        document.querySelector(".italicized").style.color = "black";
+        document.querySelector("ol").style.color = "black";
+
+        // changes image
+        document.querySelector("img").setAttribute("src", "byui-logo-blue.webp");
+    }
+}           
+                    
