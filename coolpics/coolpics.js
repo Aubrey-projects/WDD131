26959a4116befe99988b39a09d3f7433a8f1119c
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-05-16 18:23:44 -0600

    Finished the modal part of the assignment

---
+++ coolpics/coolpics.js
@@ -0,0 +1,22 @@
+const gallery = document.querySelector('.grid_container');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(event) {
+    let imageSrc = event.target.src;
+    modalImage.src = imageSrc.replace("-sm", "-full");
+    modal.showModal();
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
