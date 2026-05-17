d7d92c1f3d15f18c69e3a5975aa22c668c782c4b
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-05-16 18:36:50 -0600

    Finished the menu button

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,3 +1,4 @@
+// Adding a modal
 const gallery = document.querySelector('.grid_container');
 const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
@@ -20,3 +21,13 @@ modal.addEventListener('click', (event) => {
         modal.close();
     }
 });
+
+// Making the menu open
+let menuButton = document.querySelector('.menu');
+menuButton.addEventListener("click", handleMenuButtonClick);
+
+function handleMenuButtonClick(event) {
+    let navigationMenu = document.querySelector("nav");
+    navigationMenu.classList.toggle("hide");
+    menuButton.classList.toggle("change");
+}
