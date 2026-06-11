17e715a00452d1b66f596773babac28b2e4f163d
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-06-11 10:30:13 -0600

    Deleted the duplicate

---
+++ dice-game-example-main/dice-game-example-main/game.js
@@ -0,0 +1,5 @@
+// when the button is clicked
+// change all the dice images to the gif animation
+document.getElementById("rollButton").addEventListener("click", event => {
+    console.log("yay it worked");
+})
