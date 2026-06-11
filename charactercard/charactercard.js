25072067d7ca5cc358ff2e2840eb21b3e94e5e69
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-06-11 16:15:15 -0600

    Got the very basic outline of the charactercard assignment made

---
+++ charactercard/charactercard.js
@@ -0,0 +1,14 @@
+const characterCard = {
+    name: "John",
+    class: "Bard",
+    level: 5,
+    health: 100,
+    image: "",
+    attacked: function() {
+
+    },
+    levelUp: function() {
+        
+    }
+
+}
