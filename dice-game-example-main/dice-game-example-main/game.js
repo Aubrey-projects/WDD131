524ee40771ff8e298e3287ba04f9bbfe75b2f90b
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-06-11 11:01:27 -0600

    finished the rest of the die game in class

--- dice-game-example-main/dice-game-example-main/game.js
+++ dice-game-example-main/dice-game-example-main/game.js
@@ -1,5 +1,32 @@
 // when the button is clicked
 // change all the dice images to the gif animation
 document.getElementById("rollButton").addEventListener("click", event => {
-    console.log("yay it worked");
-})
+    // get the images
+    const images = document.querySelectorAll("#gameboard img");
+    // change the src
+    images.forEach(image => {
+        if(isDieUnlocked(image)) {
+            image.src = "assets/die_rolling.gif";
+        }
+        
+    });
+
+    // wait half a second, then call this anonymous function
+    setTimeout(() => {
+        images.forEach(image => {
+            if(isDieUnlocked(image)) {
+                image.src = "assets/white_dice_" + (Math.floor(Math.random() * 6) + 1) + ".gif";
+            }
+        });
+    }, 500);
+
+});
+
+function isDieUnlocked(dieImage) {
+    // retrieve list of checkboxes
+    const checkboxes = document.querySelectorAll("#gameboard input");
+    // filter out checked
+    const unchecked = Array.from(checkboxes).filter(checkbox => !checkbox.checked);
+    // compare list to dieImage, if there's a match, return true
+    return unchecked.find(unchecked => unchecked.className === dieImage.className);
+}
