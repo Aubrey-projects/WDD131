e22f4de6593bb52106617a29a23d804b44943d33
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-07-04 23:20:08 -0600

    Made my personal project

---
+++ personal-folder/javascript.js
@@ -0,0 +1,10 @@
+// create the books with all of their information as objects
+const books = []
+
+// make a search function that looks at the title, description, and book tags
+
+// add an event listener for the search bar
+
+// render the books
+
+// make script for the multiple choice trivia on home page
